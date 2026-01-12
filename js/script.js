7dd8a1036872957ed16bed7897efb32bc463f42a
MasterCraftGamer2025 <mcgamezone2025@gmail.com>
2026-01-12 18:58:27 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("mouseover", () => {
+    link.style.textShadow = "0 0 10px lime";
+  });
+  link.addEventListener("mouseout", () => {
+    link.style.textShadow = "none";
+  });
+});
